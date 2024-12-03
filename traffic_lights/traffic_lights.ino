0c3c74ed14972f0012c911b7b782c14cdee4e1ab
Tiger <billath131908@gmail.com>
2024-12-03 02:49:47 +0200

    OK

--- traffic_lights/traffic_lights.ino
+++ traffic_lights/traffic_lights.ino
@@ -2,38 +2,46 @@
 SoftwareSerial espSerial(6, 7);   // Connect (Arduino RX: pin 6) to (ESP-01: TX pin)
                                   // and (Arduino TX: pin 7) to (ESP-01: RX pin)
 #define DEBUG true
-String mySSID = "linksys_1";               // WiFi SSID
-String myPWD = "";                // WiFi Password
-String myAPI = "7X90Y7VEBTKVZVBO";
-String otherUserApi = "";
+String mySSID = "linksys_1";            // WiFi SSID
+String myPWD = "";                	    // WiFi Password
+String myWriteAPI = "6UKE7N1W16R0TIOC"; // Write API Key
 
-String myReadApi = "DTD07IEHUEAZD9BH";             // Read API Key
-String myHOST = "api.thingspeak.com";
-String myPORT = "80";
-String myCHANNEL = "1923877";
+String myReadAPI = "MCX66QIC4S1OR75T";  // Read API Key
+String myHOST = "api.thingspeak.com";   // ThingSpeak Host
+String myPORT = "80";                   // ThingSpeak Port
+String myCHANNEL = "2749755";           // Channel ID
 
-String response = "";
+String response = "";                   // Response from ESP8266
 
-String field = "field2";
-String alertField = "field8";
+String fieldRed = "field1";             // Field for RED signal
+String fieldOrange = "field2";          // Field for ORANGE signal
+String fieldGreen = "field3";           // Field for GREEN signal
+String alertField = "field8";           // Field for ALERT signal
+
+int DELAY_RED = 30000;                  // Delay for RED signal
+int DELAY_ORANGE = 20000;               // Delay for ORANGE signal
+int DELAY_GREEN = 30000;                // Delay for GREEN signal
+String tr_light = "";                   // Traffic light status
+char path[100];                         // Path for ThingSpeak
+int i = 0;                              // Counter
  
-int sendVal;
-String sendData;
-String sendDataToOtherUser;
-String res_feeds = "";
-unsigned int delayVal = 15000;
-String messager;
-String x01;
+int sendVal;                            // Value to be sent
+String sendData = "";                   // Data to be sent
+String res_feeds = "";                  // Response from ThingSpeak
+unsigned int delayVal = 15000;          // Delay value
+String messager;                        // Message to be sent
+String x01;                             // Value of alert field
 
-int currentMillis;
-int previousMillis = 0;
-const unsigned long timer = 600000 // 10 minutes
+int currentMillis;                      // Current time     
+int previousMillis = 0;                 // Previous time
+const unsigned long timer = 600000      // 10 minutes
 
 void setup()
 {
   Serial.begin(9600);
-  espSerial.begin(9600);                                              // IN CASE OF ERROR, change espSerial to 9600
+  espSerial.begin(9600);                                               // IN CASE OF ERROR, change espSerial to 9600
  
+  Serial.println("======== [ESP8266 Setup] ========");
   espData("AT+RST", 1000, DEBUG);                                      // ESP-01 Reset
   espData("AT+CWMODE=1", 1000, DEBUG);                                 // Mode=1 => client
   espData("AT+CWJAP=\""+ mySSID +"\",\""+ myPWD +"\"", 1000, DEBUG);   // Connect to WiFi network
@@ -46,20 +54,53 @@ void setup()
       Serial.print(".");
       delay(1000);
   }
+ 
   Serial.println("Connected!");
+  Serial.println("=================================");
   delay(1000);
  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
- 
-  // Zitoumeno A.4
+void loop() 
+{
+  Serial.println("======== [Task A.4] ========");
+  
+  // RED Signal
+  sendVal = 1;
+  tr_light="RED";                         
+  sendData = "GET /update?api_key=" + myWriteAPI + "&" + fieldRed + "=" + String(sendVal);
+  espData("AT+CIPMUX=1", 1000, DEBUG);   
+  espData("AT+CIPSTART=0,\"TCP\",\"" + myHOST + "\"," + myPORT, 1000, DEBUG);
+  espData("AT+CIPSEND=0," + String(sendData.length() + 4), 1000, DEBUG);  
+  espSerial.find(">");
+  espSerial.println(sendData);
+  Serial.print("Value to be sent: ");
+  Serial.println(sendVal);
+     
+  espData("AT+CIPCLOSE=0",10000,DEBUG);
+  Serial.println(tr_light);
+  delay(DELAY_RED); // 30 sec
 
-  // turn on red
-  sendVal = 0;                            // 0 for red
-  sendData = "GET /update?api_key="+ myAPI +"&"+ field +"="+String(sendVal);
-  espData("AT+CIPMUX=1", 1000, DEBUG);               // Allow multiple connections
+  // ORANGE Signal
+  sendVal = 2;
+  tr_light="ORANGE";    						         
+  sendData = "GET /update?api_key="+ myWriteAPI + "&" + fieldOrange + "=" + String(sendVal);
+  espData("AT+CIPMUX=1", 1000, DEBUG);     
+  espData("AT+CIPSTART=0,\"TCP\",\"" + myHOST + "\"," + myPORT, 1000, DEBUG);
+  espData("AT+CIPSEND=0," + String(sendData.length() + 4), 1000, DEBUG); 
+  espSerial.find(">");
+  espSerial.println(sendData);
+  Serial.print("Value to be sent: ");
+  Serial.println(sendVal);
+     
+  espData("AT+CIPCLOSE=0",10000,DEBUG);
+  Serial.println(tr_light);
+  delay(DELAY_ORANGE); // 20 sec 
+
+  // GREEN Signal
+  sendVal = 2;    
+  tr_light="GREEN";                                   
+  espData("AT+CIPMUX=1", 1000, DEBUG);              
   espData("AT+CIPSTART=0,\"TCP\",\""+ myHOST +"\","+ myPORT, 1000, DEBUG);
   espData("AT+CIPSEND=0," +String(sendData.length()+4),1000,DEBUG);  
   espSerial.find(">");
@@ -68,47 +109,28 @@ void loop() {
   Serial.println(sendVal);
      
   espData("AT+CIPCLOSE=0",10000,DEBUG);
-  delay(3000); // 30 sec
+  Serial.println(tr_light);
+  delay(DELAY_GREEN); // 30 sec
 
-  // turn on orange
-  sendVal = 1;    // 1 for orange
-  sendData = "GET /update?api_key="+ myAPI +"&"+ field+"="+String(sendVal);
-  espData("AT+CIPMUX=1", 1000, DEBUG);               // Allow multiple connections
-  espData("AT+CIPSTART=0,\"TCP\",\""+ myHOST +"\","+ myPORT, 1000, DEBUG);
-  espData("AT+CIPSEND=0," +String(sendData.length()+4),1000,DEBUG);  ε
-  espSerial.find(">");
-  espSerial.println(sendData);
-  Serial.print("Value to be sent: ");
-  Serial.println(sendVal);
-     
-  espData("AT+CIPCLOSE=0",10000,DEBUG);
-  delay(2000); // 20 sec 
+  Serial.println("============================"); 
 
-  // turn on green
-  sendVal = 2;                            // 2 for green
-  espData("AT+CIPMUX=1", 1000, DEBUG);               // Allow multiple connections
-  espData("AT+CIPSTART=0,\"TCP\",\""+ myHOST +"\","+ myPORT, 1000, DEBUG);
-  espData("AT+CIPSEND=0," +String(sendData.length()+4),1000,DEBUG);  
-  espSerial.find(">");
-  espSerial.println(sendData);
-  Serial.print("Value to be sent: ");
-  Serial.println(sendVal);
-     
-  espData("AT+CIPCLOSE=0",10000,DEBUG);
-  delay(3000); // 30 sec
 
-  // Anti gia 0 vazoume 3 kai anti 1 vazoume 4
+  Serial.println("======== [Task B.1] ========");
 
-  // zitoumeno b.1
-  sendVal = 3;                            
-  sendData = "GET /update?api_key="+ otherUserApi +"&"+ alertField +"="+String(sendVal);
-  espData("AT+CIPMUX=1", 1000, DEBUG);               // Allow multiple connections
-  espData("AT+CIPSTART=0,\"TCP\",\""+ myHOST +"\","+ myPORT, 1000, DEBUG);
-  espData("AT+CIPSEND=0," +String(sendData.length()+4),1000,DEBUG);  
-  espSerial.find(">");
-  espSerial.println(sendData);
-  Serial.print("Value to be sent: ");
-  Serial.println(sendVal);
+  while(1)
+  {
+    if (Serial.available() > 0) 
+    { // Checks whether data is comming from the serial port
+      path[i] = Serial.read(); // Reads the data from the serial port
+      Serial.print(path[i]);
+      Serial.print('\t');
+    }
+    
+    if (path[i] == 'e')
+    {
+      Serial.print("loop exit");
+      break;
+    }
 
   // zitoumeno b.2
   sendVal = 3;                            
@@ -195,7 +217,7 @@ void loop() {
  
 }
 
- String espData(String command, const int timeout, boolean debug)
+String espData(String command, const int timeout, boolean debug)
 {
   Serial.print("AT Command ==> ");
   Serial.print(command);
@@ -214,7 +236,7 @@ void loop() {
   }
   if (debug)
   {
-    //Serial.print(response);
+    Serial.print(response);
   }
   return response;
 }
